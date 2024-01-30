68b29164cbe7ff76a97911b0648d9fef8197b0c7
Jiří Klepl <klepl@d3s.mff.cuni.cz>
2024-01-30 12:14:13 +0100

    improve

--- playground.cpp
+++ playground.cpp
@@ -1,8 +1,8 @@
 #include <noarr/structures_extended.hpp>
 #include <noarr/structures/interop/bag.hpp>
 #include <noarr/structures/extra/traverser.hpp>
-
+#include <noarr/structures/extra/shortcuts.hpp>
 
 int main() {
-
+    // play with noarr...
 }
