525dd67123bada65321547410a273104e6fa4481
Jiří Klepl <klepl@d3s.mff.cuni.cz>
2024-01-30 04:05:14 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Noarr Playground
+
+## How to build
+
+```bash
+mkdir build
+cd build
+cmake ..
+cmake --build .
+```
+
+## How to run
+
+```bash
+./noarr_playground
+```
---
+++ playground.cpp
@@ -0,0 +1,8 @@
+#include <noarr/structures_extended.hpp>
+#include <noarr/structures/interop/bag.hpp>
+#include <noarr/structures/extra/traverser.hpp>
+
+
+int main() {
+
+}
